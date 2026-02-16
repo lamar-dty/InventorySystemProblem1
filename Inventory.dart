7a6a945e4a1b0e8abbbb00960678fde6668badd6
lamar-dty <junlaspinas9@gmail.com>
2026-02-16 17:37:28 +0800

    Second commit

--- Inventory.dart
+++ Inventory.dart
@@ -1,2 +1,16 @@
 import 'dart:io';
 
+void main() {
+
+  while (true) {
+    print("\n==== Inventory Menu ====");
+    print("1. Add Product");
+    print("2. Display Products");
+    print("3. Sell Product");
+    print("4. Exit");
+    stdout.write("Choose an option (1-4): ");
+
+    String? choice = stdin.readLineSync();
+
+  }
+}
